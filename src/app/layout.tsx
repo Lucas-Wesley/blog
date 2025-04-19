c5855b554b383ce2a0572ae8f4a64593cd948624
Lucas Wesley <lucaswesley9010@hotmail.com>
2025-04-19 18:43:05 -0300

    Colocando google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Documentando meus estudos em Tecnologia" />
+        <meta name="theme-color" content="#1E3A8A" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+
       <body className={notoSansMono.className}>
         <nav className="bg-gray-800 text-white p-4">
           <div className="container mx-auto">
@@ -26,6 +33,19 @@ export default function RootLayout({
           </div>
         </nav>
         {children}
+
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-211018649V"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-211018649V');
+            `,
+          }}
+        />
       </body>
     </html>
   );
